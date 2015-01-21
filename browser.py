ce8fca3c9b5747ae359f995900d3e341e7c95146
swift1911 <swift1911@swift1911-Lenovo-IdeaPad-Y480>
2015-01-21 21:39:25 +0800

    test

---
+++ browser.py
@@ -0,0 +1,14 @@
+import sys
+import array
+sys.float_info.dig 
+15
+s = '3.14159265358953'    # decimal string with 15 significant digits
+format(float(s), '.15g')  # convert to float and back -> same value
+'3.14159265358979'
+f = open("1.txt")
+try:
+    for line in f:
+        print line,
+finally:
+    f.close()
+
